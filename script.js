f1f85019ccdafed60ed7a80973fc9e3efe77d07f
Mireia Guix <mguix@users.noreply.github.com>
2019-11-10 20:02:21 +0000

    updating forcast days

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let today = new Date();
-
-let currentDate = document.querySelector("p");
-let days = [
+const today = new Date();
+const currentDate = document.querySelector("p");
+const days = [
   "Sunday",
   "Monday",
   "Tuesday",
@@ -10,7 +9,13 @@ let days = [
   "Friday",
   "Saturday"
 ];
-let months = [
+
+const shortDays = days.map(item => ({
+  name: item,
+  shortName: item.slice(0, 3)
+}));
+
+const months = [
   "January",
   "February",
   "March",
@@ -24,11 +29,11 @@ let months = [
   "November",
   "December"
 ];
-let day = days[today.getDay()];
-let month = months[today.getMonth()];
-let date = today.getDate();
-let year = today.getFullYear();
-let hours = today.getHours();
+const day = days[today.getDay()];
+const month = months[today.getMonth()];
+const date = today.getDate();
+const year = today.getFullYear();
+const hours = today.getHours();
 let minutes = today.getMinutes();
 if (minutes < 10) {
   minutes = "0" + minutes;
@@ -37,33 +42,33 @@ if (minutes < 10) {
 currentDate.innerHTML = `${hours}:${minutes}  ${day} ${date}, ${month} ${year}`;
 
 function showForecast(response) {
-  let nextDayTemp = document.querySelector("#next-day-temp");
+  const nextDayTemp = document.querySelector("#next-day-temp");
   nextDayTemp.innerHTML = Math.round(response.data.list[12].main.temp) + "º";
-  let nextDayIcon = document.querySelector("#next-day-icon");
+  const nextDayIcon = document.querySelector("#next-day-icon");
   nextDayIcon.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.list[12].weather[0].icon}@2x.png`
   );
 
-  let secondDayTemp = document.querySelector("#second-day-temp");
+  const secondDayTemp = document.querySelector("#second-day-temp");
   secondDayTemp.innerHTML = Math.round(response.data.list[20].main.temp) + "º";
-  let secondDayIcon = document.querySelector("#second-day-icon");
+  const secondDayIcon = document.querySelector("#second-day-icon");
   secondDayIcon.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.list[20].weather[0].icon}@2x.png`
   );
 
-  let thirdDayTemp = document.querySelector("#third-day-temp");
+  const thirdDayTemp = document.querySelector("#third-day-temp");
   thirdDayTemp.innerHTML = Math.round(response.data.list[28].main.temp) + "º";
-  let thirdDayIcon = document.querySelector("#third-day-icon");
+  const thirdDayIcon = document.querySelector("#third-day-icon");
   thirdDayIcon.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.list[28].weather[0].icon}@2x.png`
   );
 
-  let forthDayTemp = document.querySelector("#forth-day-temp");
+  const forthDayTemp = document.querySelector("#forth-day-temp");
   forthDayTemp.innerHTML = Math.round(response.data.list[36].main.temp) + "º";
-  let forthDayIcon = document.querySelector("#forth-day-icon");
+  const forthDayIcon = document.querySelector("#forth-day-icon");
   forthDayIcon.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.list[36].weather[0].icon}@2x.png`
@@ -71,74 +76,81 @@ function showForecast(response) {
 }
 
 function search(searchCity) {
-  let apiKey = "a2df7199551cc39797a0929621d2b43a";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}&units=metric`;
+  const apiKey = "a2df7199551cc39797a0929621d2b43a";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 
-  let fApiKey = "a2df7199551cc39797a0929621d2b43a";
-  let fApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=${fApiKey}&units=metric`;
+  const fApiKey = "a2df7199551cc39797a0929621d2b43a";
+  const fApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=${fApiKey}&units=metric`;
   axios.get(fApiUrl).then(showForecast);
 }
 
 function changeCity(event) {
   event.preventDefault();
-  let cityInput = document.querySelector("#city-input");
-  let h2 = document.querySelector("h2");
+  const cityInput = document.querySelector("#city-input");
+  const h2 = document.querySelector("h2");
   h2.innerHTML = cityInput.value;
   search(cityInput.value);
 }
 
-let citySubmit = document.querySelector("#city-form");
+const citySubmit = document.querySelector("#city-form");
 citySubmit.addEventListener("submit", changeCity);
 
 function changeToCelcius() {
-  let temperature = document.querySelector("#temperature");
-  let celciusLink = document.querySelector("#celcius");
+  const temperature = document.querySelector("#temperature");
+  const celciusLink = document.querySelector("#celcius");
   celciusLink.classList.add("active");
-  let cLink = document.querySelector("#farn");
+  const cLink = document.querySelector("#farn");
   cLink.classList.remove("active");
   temperature.innerHTML = Math.round(cecliusTemperature);
 }
 
 function changeToFarn(event) {
   event.preventDefault();
-  let temperature = document.querySelector("#temperature");
-  let farnLink = document.querySelector("#farn");
+  const temperature = document.querySelector("#temperature");
+  const farnLink = document.querySelector("#farn");
   farnLink.classList.add("active");
-  let fLink = document.querySelector("#celcius");
+  const fLink = document.querySelector("#celcius");
   fLink.classList.remove("active");
   temperature.innerHTML = Math.round((cecliusTemperature * 9) / 5 + 32);
 }
 
-let celciusLink = document.querySelector("#celcius");
+const celciusLink = document.querySelector("#celcius");
 celciusLink.addEventListener("click", changeToCelcius);
 
-let farnLink = document.querySelector("#farn");
+const farnLink = document.querySelector("#farn");
 farnLink.addEventListener("click", changeToFarn);
 
 let cecliusTemperature = null;
 
 function showTemperature(response) {
-  let temperature = document.querySelector("#temperature");
+  const temperature = document.querySelector("#temperature");
   temperature.innerHTML = Math.round(response.data.main.temp);
-  let city = document.querySelector("#city-name");
+
+  const city = document.querySelector("#city-name");
   city.innerHTML = response.data.name;
-  let weatherIcon = document.querySelector("#weatherIcon");
+
+  const weatherIcon = document.querySelector("#weatherIcon");
   cecliusTemperature = response.data.main.temp;
+
   weatherIcon.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   weatherIcon.setAttribute("alt", response.data.weather[0].description);
 
-  let currentDays = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-  let nextDay = document.querySelector("#next-day");
-  nextDay.innerHTML = currentDays[today.getDay() + 1];
-  let secondDay = document.querySelector("#second-day");
-  secondDay.innerHTML = currentDays[today.getDay() + 4];
-  if (secondDay > 6) {
-    return 0;
+  let count = today.getDay() + 1;
+  if (count === 7) {
+    count = 1;
   }
+
+  document.getElementById("next-day").innerHTML = shortDays[count].shortName;
+  count++;
+  document.getElementById("second-day").innerHTML = shortDays[count].shortName;
+  count++;
+  document.getElementById("third-day").innerHTML = shortDays[count].shortName;
+  count++;
+  document.getElementById("forth-day").innerHTML = shortDays[count].shortName;
 }
 
 search("Barcelona");
